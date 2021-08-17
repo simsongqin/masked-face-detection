7c4ce29232d4e4378d5cff0add0007ae3c46129c
simsongqin <57763201+simsongqin@users.noreply.github.com>
2021-08-17 09:47:45 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ from __future__ import print_function, division
 import os
 import argparse
 import time
-import  pdb
+import pdb
 import math
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 # torch 包
 import torch
@@ -23,9 +23,11 @@ from dataset import ImageList
 from net import sphere20
 from layer import MarginCosineProduct
 import LResnet50
+import sphere
+import nfold_eval
+import lfw_eval
 
-DATASET_ROOT = '/export/home/iceicehyhy/CASIA-maxpy-clean/'
-
+DATASET_ROOT = '/home/FYP/sims0038/CASIA-maxpy-clean'
 """
 Desc:
     complete the training process
@@ -42,17 +44,17 @@ Contact:
 # training parameter
 parser = argparse.ArgumentParser('Pure implementation of CosFace by Pytorch')
 parser.add_argument('--root-path', type=str, default= DATASET_ROOT , help='the training set root path')
-parser.add_argument('--image-list', type=str, default='/export/home/iceicehyhy/CosFace_Adaptive_S_1/Casia_Webface_112x112_train_list.txt', help='the file and its path of image list')
-parser.add_argument('--batch-size', type=int, default=128)
+parser.add_argument('--image-list', type=str, default='Casia_Webface_112x112_train_list.txt', help='the file and its path of image list')
+parser.add_argument('--batch-size', type=int, default=512)
 parser.add_argument('--num-class', type=int, default=10575, help='number of people(class)')
-parser.add_argument('--epochs', type=int, default=30)
-parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--epochs', type=int, default=50)
+parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--momentum', type=float, default=0.9)
 parser.add_argument('--weight-decay', type=float, default=5e-4)
-parser.add_argument('--log-interval', type=float, default=20)
-#parser.add_argument('--step-size', type=int, default=[16000, 24000])
-parser.add_argument('--step-size', type=int, default=[55000, 70775, 99085])
-parser.add_argument('--save-path', type=str, default='checkpoints')
+parser.add_argument('--log-interval', type=float, default=100)
+parser.add_argument('--step-size', type=int, default=[100006000])
+#parser.add_argument('--step-size', type=int, default=[55000, 70775, 99085])
+parser.add_argument('--save-path', type=str, default='chkpt_1')
 parser.add_argument('--no-cuda', type=bool, default=False)
 parser.add_argument('--workers', type=int, default=4)
 parser.add_argument('--gpus', type=str, default='0,1,2,3,4,5,6,7')
@@ -83,15 +85,17 @@ def train(train_loader, model, MCP, criterion, optimizer, epoch):
         # compute output
         output = model(data)
         # pdb.set_trace()
-        output = MCP(output, target).requires_grad_()   # allomem for gradients
+        #output = MCP(output, target).requires_grad_()   # allomem for gradients
+        output = MCP(output, target)
 
         # print('------------------')
         # print('output of MCP = ', output.shape)
         # print('target = ', target.shape)
         # print('---------------------------')
         loss = criterion(output, target)
+        loss_display += loss.item()
 
-        loss_display += loss.detach().item()
+        #loss_display += loss.detach().item()
         # compute gradient and do SGD step
         optimizer.zero_grad()
         # back propagation
@@ -117,7 +121,6 @@ def train(train_loader, model, MCP, criterion, optimizer, epoch):
             time_curr = time.time()
             loss_display = 0.0
 
-
 def print_with_time(string):
     print(time.strftime('%Y-%M-%d %H:%M:%S', time.localtime()) + string)
 
@@ -128,17 +131,17 @@ def main():
         print('available gpus is ', torch.cuda.device_count(), torch.cuda.get_device_name())
     else:
         print("only one GPU found !!!")
-    #model = sphere20()
+    #model = sphere.sphere(type=20, is_gray=False)
     model = LResnet50.LResNet50E_IR(is_gray = False)
-    print (model)
+    #print (model)
     model = torch.nn.DataParallel(model, device_ids = [0],output_device=0).cuda()   # enable mutiple-gpu training
 
-    # print(model)
+    print(model)
 
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
     # model.save(args.save_path + '/CosFace_0_checkpoint.pth')
-
+cd 
     print('save checkpoint finished!')
 
     # upload training dataset
@@ -172,7 +175,7 @@ def main():
     # --------------------------------loss function and optimizer-------------------------------
     # core implementation of Cos face, using cuda
     scale = math.sqrt(2) * math.log(args.num_class - 1)
-    MCP = MarginCosineProduct(512, args.num_class, s=scale).cuda()
+    MCP = MarginCosineProduct(512, args.num_class).cuda()
 
     criterion = torch.nn.CrossEntropyLoss().cuda()
 
@@ -183,18 +186,20 @@ def main():
         weight_decay=args.weight_decay
     )
     if resume:
-        print ("resume from epoch 10!!!")
-        pretrained_cnn = torch.load('./checkpoints/CosFace_10_checkpoint.pth')
-        pretrained_mcp = torch.load('./checkpoints/MCP_10_checkpoint.pth') 
+        print ("resume from epoch 22!!")
+        pretrained_cnn = torch.load('./chkpt_1/CosFace_21_checkpoint.pth')
+        pretrained_mcp = torch.load('./chkpt_1/MCP_21_checkpoint.pth') 
         model.load_state_dict(pretrained_cnn)
         MCP.load_state_dict(pretrained_mcp)
 
-    for epoch in range(10, args.epochs + 1):
+    for epoch in range(22, args.epochs + 1):
         train(train_loader, model, MCP, criterion, optimizer, epoch)
-        if (epoch % 5 == 0):
+        if (epoch % 1 == 0):
             torch.save(model.state_dict(), os.path.join(args.save_path, 'CosFace_' + str(epoch) + '_checkpoint.pth'))
             torch.save(MCP.state_dict(), os.path.join(args.save_path, 'MCP_' + str(epoch) + '_checkpoint.pth'))
-
+        print("Evaluation result after epoch "+str(epoch)+":")
+        nfold_eval.eval(args.save_path+'/'+'CosFace_' + str(epoch) + '_checkpoint.pth')
+        #lfw_eval.eval(LResnet50.LResNet50E_IR(is_gray=False), model_path=args.save_path+'/'+'CosFace_' + str(epoch) + '_checkpoint.pth')
     print('Finished Training')
 
 
@@ -208,9 +213,12 @@ def adjust_learning_rate(optimizer, iteration, step_size):
     :return:
     """
     if iteration in step_size:
+        print()
+        print("Iteration: "+str(iteration))
         lr = args.lr * (0.1 ** (step_size.index(iteration) + 1))
+  
         print_with_time('Adjust learning rate to {}'.format(lr))
-
+        print()
         #  managing parameters using param_groups in optimizer， param_group
         #  which including parameter group, corresponding lr, momentum etc.
         for param_group in optimizer.param_groups:
